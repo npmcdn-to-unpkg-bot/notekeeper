eb856f833f9c7a07f701ff8511e672bbeaccd9f8
Sajad Torkamani <sajadtorkamani1@gmail.com>
2016-06-26 18:08:49 +0100

    Add model spec for Note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,8 @@
 class Note < ActiveRecord::Base
   belongs_to :user
   belongs_to :colour
+
+  validates :title, { presence: true }
+
+  validates :user, { presence: true }
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    valid_note = build(:note)
+    expect(valid_note).to be_valid
+  end
+
+  it "has an invalid factory called invalid_note" do
+    invalid_note = build(:invalid_note) 
+    expect(invalid_note).to be_invalid
+  end
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:user) }
+
 end
